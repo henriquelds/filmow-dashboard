3eb3ff2f0742981981d0237d91c7dc5f00b0a80a
henriquelds <henriquelds94@gmail.com>
2019-11-13 21:09:28 -0200

    Added recommender module (CF) - TODO: a LOT of improvements

--- server.R
+++ server.R
@@ -9,6 +9,8 @@
 
 library(shiny)
 library(ggplot2)
+library(recommenderlab)
+library(Matrix)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -85,4 +87,40 @@ server <- function(input, output) {
             geom_density(alpha=.2, fill="#FF6666") + labs(x = "Rating", y = "Density")
         return(p)   
     })
+    
+    recdata <- eventReactive(input$recButton,{
+        query <- sqlInterpolate(ANSI(), "SELECT rats.user_id, movs.title, rats.rating as rating
+                FROM public.ratings as rats
+                INNER JOIN public.movies as movs ON movs.movie_tag = rats.movie_tag
+                LIMIT ?lim;", lim=input$numRats)
+        outp <- dbGetQuery(pool, query)
+
+        query2 <- sqlInterpolate(ANSI(), "SELECT rats.user_id, movs.title, rats.rating as rating
+                FROM public.ratings as rats
+                INNER JOIN public.movies as movs ON movs.movie_tag = rats.movie_tag
+                INNER JOIN public.users as users ON rats.user_id = users.id AND users.username = ?username
+                LIMIT 2000;", username = input$textUserRec)
+        outp2 <- dbGetQuery(pool, query2)
+        pivotid <- outp2$user_id[1]
+        train <- rbind(outp,outp2)
+        refid <- unique(train$user_id)
+        pivotindex <- which(refid == pivotid)
+        train$user_id <- as.factor(train$user_id)
+        train$title <- as.factor(train$title)
+        train$rating <- as.numeric(train$rating)
+        train <- as(train, "realRatingMatrix")
+        model2 <- Recommender(data = train, method="IBCF", parameter=list(k=5, method="Cosine"))
+        b <- as(predict(model2,pivotindex,data=train, n=input$numRecs), "list")
+        return(b[[1]])
+    })
+    
+    output$rectable <- DT::renderDataTable({
+        l <- recdata()
+        df <- data.frame(matrix(unlist(l), nrow=length(l), byrow=T))
+        names(df)[1] <- "Title"
+        ret <- DT::datatable(df)
+        return(ret)
+    })
+    
+    
 }
--- ui.R
+++ ui.R
@@ -16,7 +16,8 @@ ui <- dashboardPage(
     dashboardSidebar(sidebarMenu(
         menuItem("Movies Visualizer", tabName = "movvisu", icon = icon("dashboard")),
         menuItem("Density plots (movies)", tabName = "densmov", icon = icon("th")),
-        menuItem("Density plots (users)", tabName = "densuser", icon = icon("th"))
+        menuItem("Density plots (users)", tabName = "densuser", icon = icon("th")),
+        menuItem("Recommender (CF)", tabName = "rec", icon = icon("th"))
     )),
     dashboardBody(
         tabItems(
@@ -53,12 +54,26 @@ ui <- dashboardPage(
                         textInput("textUser", "Type username:", value="heinrique"),
                         sliderInput("slideryearpd", "Select year range:", min=data_maxmin$min, max=data_maxmin$max, value = c(data_maxmin$min, data_maxmin$max),step=10),
                         #selectizeInput("selectizedir", "Select director:", choices=directors$name, options = list(maxItems = 1)),
-                        actionButton("runButton", "Plot"),
+                        actionButton("runButton", "Run"),
                         width = 12)
                     )
                 ),
                 fluidRow(
                     column(8,plotOutput("plotdensuseryear", height = 250)),  
+                )
+            ),
+            tabItem(tabName = "rec",
+                fluidRow(
+                    column(6,box(DT::dataTableOutput("rectable",width="80%"),width = 12)),
+                    column(6,box(
+                        title = "Controls",
+                        textInput("textUserRec", "Type username:", value="heinrique"),
+                        numericInput("numRats", "Select amount of ratings to be considered:", min=1000, max=20000, value = 5000, step=1000),
+                        numericInput("numRecs", "Select amount of recommendations:", min=1, max=50, value = 10, step=1),
+                        #selectizeInput("selectizedir", "Select director:", choices=directors$name, options = list(maxItems = 1)),
+                        actionButton("recButton", "Run"),
+                        width = 12)
+                    )
                 )
             )
         )
